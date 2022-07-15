c9ca754ae6dddabfc6dbe64cbd100b11bbe275bb
GhostNear <ghostnear1841@gmail.com>
2022-07-16 02:15:48 +0300

    Added buttons with text

--- src/app.v
+++ src/app.v
@@ -1,5 +1,6 @@
 module main
 
+import gx
 import vstorm
 
 fn app_init(mut app &vstorm.StormContext) {
@@ -7,9 +8,98 @@ fn app_init(mut app &vstorm.StormContext) {
 	mut bkg := create_background()
 	app.root.add_child(mut bkg, 'background')
 
-	// Test a button
-	mut b_test := create_calculator_button()
-	bkg.add_child(mut b_test, 'test_button')
+	// Create the calculator buttons
+	text := [
+		['+', '-', '(', ')'],
+		['7', '8', '9', '*'],
+		['4', '5', '6', '/'],
+		['1', '2', '3'],
+		['.', '0'],
+	]
+	mut i := 0
+	for x in text {
+		mut j := 0
+		for y in x {
+			if y != '' {
+				bkg.add_child(
+					mut create_calculator_button(
+						ButtonConfig{
+							yindex: i
+							xindex: j
+							text: y
+							normal_color: gx.Color{
+								r: 0x22
+								g: 0x22
+								b: 0x22
+							}
+							over_color: gx.Color{
+								r: 0x33
+								g: 0x33
+								b: 0x33
+							}
+							size: vstorm.NodeV2D{
+								x: 1
+								y: 1
+							}
+						}
+					),
+					'calc_button_${i}_${j}'
+				)
+			}
+			j++
+		}
+		i++
+	}
+
+	// Special buttons ofc
+	bkg.add_child(
+		mut create_calculator_button(
+			ButtonConfig{
+				xindex: 3
+				yindex: 3
+				text: '='
+				normal_color: gx.Color{
+					r: 0x22
+					g: 0x44
+					b: 0x44
+				}
+				over_color: gx.Color{
+					r: 0x33
+					g: 0x66
+					b: 0x66
+				}
+				size: vstorm.NodeV2D{
+					x: 1
+					y: 2
+				}
+			}
+		),
+		'calc_button_equals'
+	)
+	bkg.add_child(
+		mut create_calculator_button(
+			ButtonConfig{
+				xindex: 2
+				yindex: 4
+				text: '←'
+				normal_color: gx.Color{
+					r: 0x44
+					g: 0x22
+					b: 0x22
+				}
+				over_color: gx.Color{
+					r: 0x66
+					g: 0x33
+					b: 0x33
+				}
+				size: vstorm.NodeV2D{
+					x: 1
+					y: 1
+				}
+			}
+		),
+		'calc_button_remove_one'
+	)
 }
 
 fn main() {
--- src/factories.v
+++ src/factories.v
@@ -1,6 +1,7 @@
 module main
 
 import gx
+import math
 import vstorm
 
 // Background factory
@@ -16,7 +17,7 @@ pub fn create_background() &vstorm.Node{
 	)
 	node.add_function(
 		fn(mut node &vstorm.Node) {
-			mut ggc := &node.context.win.gg
+			mut ggc := node.context.win.gg
 			w_size := node.context.win.get_size()
 			ggc.draw_rect_filled(
 				0, 0,
@@ -29,47 +30,166 @@ pub fn create_background() &vstorm.Node{
 	return node
 }
 
+// Config for the button texts
+struct ButtonTextConfig {
+	text string
+	textcfg gx.TextCfg
+}
+
+// Factory for BUTTON texts
+pub fn create_text(text string) &vstorm.Node {
+	mut node := &vstorm.Node{}
+	node.add_component(
+		&ButtonTextConfig {
+			text: text
+			textcfg: gx.TextCfg{
+				color: gx.rgb(0xAA, 0xAA, 0xAA)
+				size: 40
+				italic: true
+				align:          gx.HorizontalAlign.center
+				vertical_align: gx.VerticalAlign.middle
+			}
+		},
+		'text'
+	)
+	node.add_function(
+		fn(mut node &vstorm.Node) {
+			mut win := node.context.win
+			mut ggc := win.gg
+			w_size := win.get_size()
+
+			// Parent is guaranteed to be a button so get the properties so we know where to draw the text
+			pos := (&vstorm.NodeV2D(node.parent.get_component('position'))).get_relative_to(w_size)
+			siz := (&vstorm.NodeV2D(node.parent.get_component('size'))).get_relative_to(w_size)
+
+			// Get text config and size
+			t := &ButtonTextConfig(node.get_component('text'))
+			ggc.set_cfg(t.textcfg)
+
+			ggc.draw_text(
+				int(pos.x + siz.x / 2),
+				int(pos.y + siz.y / 2),
+				t.text,
+				t.textcfg
+			)
+		},
+		'draw'
+	)
+	return node
+}
+
+// Config for the factory
+struct ButtonConfig {
+	xindex int
+	yindex int
+	text string
+	size vstorm.NodeV2D
+	normal_color gx.Color
+	over_color gx.Color
+}
+
+struct ButtonAnimState {
+mut:
+	mouse_over bool
+	normal_color gx.Color
+	over_color gx.Color
+}
+
 // Button factory
-pub fn create_calculator_button() &vstorm.Node {
+pub fn create_calculator_button(cfg ButtonConfig) &vstorm.Node {
 	mut butt := &vstorm.Node{}
+	mut size_x := f32(135.0 / 540)
+	mut size_y := f32(135.0 / 960)
 	butt.add_component(
 		&vstorm.NodeV2D {
-			x: 0
-			y: 0.5
+			x: 0	   			+ size_x * cfg.xindex
+			y: (285.0 / 960)  	+ size_y * cfg.yindex
 			r: true
 		},
 		'position'
 	)
 	butt.add_component(
 		&vstorm.NodeV2D {
-			x: 0.25
-			y: 0.25
+			x: size_x * cfg.size.x
+			y: size_y * cfg.size.y
 			r: true
 		},
 		'size'		
 	)
+	butt.add_component(
+		&vstorm.NodeV2D {
+			x: 2.0
+			y: 2.0
+			r: false
+		},
+		'padding'
+	)
+	butt.add_component(
+		&ButtonAnimState{
+			normal_color: cfg.normal_color
+			over_color: cfg.over_color
+		},
+		'animation_state'
+	)
 	butt.add_function(
 		fn(mut node &vstorm.Node) {
-			mut ggc := &node.context.win.gg
+			mut ggc := node.context.win.gg
 			w_size := node.context.win.get_size()
 
-			// Convert the position relative to the window
-			poz := (&vstorm.NodeV2D(node.get_component('position'))).get_relative_to(w_size)
-
-			// Convert the size relative to the window
+			// Convert the properties to be relative to the window
+			pos := (&vstorm.NodeV2D(node.get_component('position'))).get_relative_to(w_size)
 			siz := (&vstorm.NodeV2D(node.get_component('size'))).get_relative_to(w_size)
+			pad := (&vstorm.NodeV2D(node.get_component('padding'))).get_relative_to(w_size)
+
+			anim_state := &ButtonAnimState(node.get_component('animation_state'))
+			mut color := anim_state.normal_color
+			if anim_state.mouse_over {
+				color = anim_state.over_color
+			}
 			ggc.draw_rounded_rect_filled(
-				poz.x, poz.y,
-				siz.x, siz.y,
-				0.1 * siz.y,
-				gx.Color{
-					r: 0x22
-					g: 0x22
-					b: 0x22
-				}
+				pos.x + pad.x, pos.y + pad.y,
+				siz.x - 2 * pad.x, siz.y - 2 * pad.y,
+				0.1 * math.min(siz.x, siz.y),
+				color
 			)
 		},
 		'draw'
 	)
+	butt.add_function(
+		fn(mut node &vstorm.Node) {
+			// Get window pointer
+			mut window := node.context.win
+
+			// Only on mouse moved
+			match window.latest_event.typ {
+				.mouse_move {
+					// Get mouse position
+					mut mouse_pos := window.get_mouse_pos()
+
+					// Convert the properties to be relative to the window
+					w_size := window.get_size()
+					pos := (&vstorm.NodeV2D(node.get_component('position'))).get_relative_to(w_size)
+					siz := (&vstorm.NodeV2D(node.get_component('size'))).get_relative_to(w_size)
+					pad := (&vstorm.NodeV2D(node.get_component('padding'))).get_relative_to(w_size)
+					
+					// If inside change the state
+					mut anim_state := &ButtonAnimState(node.get_component('animation_state'))
+					if mouse_pos.x >= pos.x + pad.x && mouse_pos.y >= pos.y + pad.y &&
+					   mouse_pos.x <= pos.x + siz.x - pad.x && mouse_pos.y <= pos.y + siz.y - pad.y {
+						anim_state.mouse_over = true
+					}
+					else {
+						anim_state.mouse_over = false
+					}
+				}
+				else {}
+			}
+		},
+		'event'
+	)
+	// If we have a text, add the node here
+	if cfg.text != '' {
+		butt.add_child(mut create_text(cfg.text), 'button_text')
+	}
 	return butt
 }
